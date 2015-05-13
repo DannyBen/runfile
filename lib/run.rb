47c69269af4457961dec32f302cc6e360df02c70
DannyB <dannyb@telesofia.com>
2015-05-13 13:34:30 +0000

    convert to gem

--- lib/run.rb
+++ lib/run.rb
@@ -1,37 +1,10 @@
 #!/usr/bin/env ruby
 require 'docopt'
-
-# Determines if a shell command exists by searching for it in ENV['PATH'].
-def command_exists?(command)
-	ENV['PATH'].split(File::PATH_SEPARATOR).any? {|d| File.exist? File.join(d, command) }
-end
-
-# Returns [width, height] of terminal when detected, nil if not detected.
-# Think of this as a simpler version of Highline's Highline::SystemExtensions.terminal_size()
-def detect_terminal_size
-	if (ENV['COLUMNS'] =~ /^\d+$/) && (ENV['LINES'] =~ /^\d+$/)
-		[ENV['COLUMNS'].to_i, ENV['LINES'].to_i]
-	elsif (RUBY_PLATFORM =~ /java/ || (!STDIN.tty? && ENV['TERM'])) && command_exists?('tput')
-		[`tput cols`.to_i, `tput lines`.to_i]
-	elsif STDIN.tty? && command_exists?('stty')
-		`stty size`.scan(/\d+/).map { |s| s.to_i }.reverse
-	else
-		nil
-	end
-end
-
-
-
-
-class String
-	def wrap(length=80, character=$/)
-		lead = self[/^\s+/]
-		length -= lead.size
-		scan(/.{#{length}}|.+/).map { |x| "#{lead}#{x.strip}" }.join(character)
-	end
-end
+require 'colones'
 
 module Run
+	include Colones
+
 	class Action
 		attr :usage, :help
 
@@ -87,7 +60,7 @@ module Run
 			doc += "Commands:\n"
 			@actions.each do |name, action|
 				helpline = "      #{action.help}"
-				wrapped  = helpline.wrap width
+				wrapped  = word_wrap helpline, width
 				doc += "  #{action.usage}\n#{wrapped}"
 			end
 			doc += "\n\n"
@@ -126,8 +99,4 @@ abort "Runfile not found" unless File.exist? 'Runfile'
 load 'Runfile'
 
 @runner.run *ARGV
-
-
-
-
 
---
+++ run.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'Run'
+  s.version     = '0.1.0'
+  s.date        = '2015-05-13'
+  s.summary     = "If Rake and Docopt had a baby"
+  s.description = "An easy way to create project specific command line utilities"
+  s.authors     = ["Danny Ben Shitrit"]
+  s.email       = 'db@dannyben.com'
+  s.files       = ["lib/run.rb"]
+  s.homepage    = 'http://sector-seven.net'
+  s.license     = 'MIT'
+end
