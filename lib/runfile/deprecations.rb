24b17a4511b9e13102f80b5845a7bc94a7d5a89d
DannyB <dannyb@telesofia.com>
2017-03-31 06:49:50 +0000

    change name deprecation message

--- lib/runfile/deprecations.rb
+++ lib/runfile/deprecations.rb
@@ -4,7 +4,7 @@ module Runfile
     # The name 'name' was causing some issues in some debugging sessions 
     # with pry and some other issues. It is replaced with 'title'
     def name(name=nil)
-      say! "!txtred!You are using the deprecated directive 'name' in your Runfile.\nUse 'title' instead"
+      say! "!txtred!Warning:\n  You are using the deprecated directive 'name' in your Runfile.\n  Use 'title' instead.\n"
       Runner.instance.name = name if name
     end
   end
