ae10fcbcce1ff42149f9a10078ecdd4e3ac86921
Danny Ben Shitrit <db@dannyben.com>
2021-09-29 05:10:44 +0000

    - Add compatibility layer

---
+++ lib/runfile/compatibility.rb
@@ -0,0 +1,6 @@
+# This file provides a compatibility layer for version 0.12.0
+# Simply require 'runfile/compatibility' to have the same
+# Colsole methods (say, resay...) and ExecHandler methods (run, run_bg...)
+require 'colsole'
+include Colsole
+
