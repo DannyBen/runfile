e182efca6eec0e4b1f89e6dbbc3ea17e542b916b
Danny Ben Shitrit <db@dannyben.com>
2023-02-02 07:19:42 +0000

    - Fix inability to use 'run' as an action name

--- lib/runfile.rb
+++ lib/runfile.rb
@@ -1,13 +1,8 @@
 require 'gtx'
-require 'docopt'
+require 'docopt_ng/docopt'
 require 'colsole'
 require 'requires'
 
 requires 'runfile/exceptions'
 requires 'runfile/concerns'
 requires 'runfile'
-
-if ENV['BYEBUG']
-  require 'byebug'
-  require 'lp'
-end
